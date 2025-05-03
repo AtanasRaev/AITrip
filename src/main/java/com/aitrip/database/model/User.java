20d8923e73a433d532ae2bd2b6ee1ccc539594c2
ivandar <ivandarazhanov17a@gmail.com>
2025-05-03 22:07:32 +0300

    added User Entity

---
+++ src/main/java/com/aitrip/database/model/User.java
@@ -0,0 +1,32 @@
+package com.aitrip.database.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
