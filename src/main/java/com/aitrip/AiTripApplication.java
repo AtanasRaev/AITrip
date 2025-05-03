44ccf4a5501379e3983659828a0bfccf090b3fdc
AtanasRaev <atanaspraev@gmail.com>
2025-05-03 20:58:13 +0300

    Initial commit

---
+++ src/main/java/com/aitrip/AiTripApplication.java
@@ -0,0 +1,13 @@
+package com.aitrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTripApplication.class, args);
+    }
+
+}
