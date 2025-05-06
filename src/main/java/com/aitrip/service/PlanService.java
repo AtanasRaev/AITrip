c6e314cb8d08de4762d424ce2d890022126f5fe2
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 18:48:57 +0300

    Add savePlan method to PlanService interface
    Introduced the savePlan method to handle saving plans using a PlanCreateDTO object. The method returns a PlanPageDTO, enabling clear input-output data handling in the service layer. This prepares the service for future plan creation workflows.

--- src/main/java/com/aitrip/service/PlanService.java
+++ src/main/java/com/aitrip/service/PlanService.java
@@ -1,4 +1,8 @@
 package com.aitrip.service;
 
+import com.aitrip.database.dto.PlanCreateDTO;
+import com.aitrip.database.dto.PlanPageDTO;
+
 public interface PlanService {
+    PlanPageDTO savePlan(PlanCreateDTO planCreateDTO);
 }
