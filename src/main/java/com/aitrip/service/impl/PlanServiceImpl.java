7159989d7bc2322e2387385e0de68c1c1fdeb1bc
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 15:01:37 +0300

    Add PlanServiceImpl implementation class
    Introduce `PlanServiceImpl` to provide a concrete implementation of the `PlanService` interface. Mark the class with `@Service` for Spring's component scanning and dependency injection.

---
+++ src/main/java/com/aitrip/service/impl/PlanServiceImpl.java
@@ -0,0 +1,8 @@
+package com.aitrip.service.impl;
+
+import com.aitrip.service.PlanService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanServiceImpl implements PlanService {
+}
