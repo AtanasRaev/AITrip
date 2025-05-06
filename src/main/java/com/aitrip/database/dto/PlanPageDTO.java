49efdf91b2906467650921998edc914056cca70b
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 18:48:48 +0300

    Add PlanPageDTO class for representing travel plan data
    This commit introduces the `PlanPageDTO` class in the DTO layer. It includes fields such as origin, destination, travel dates, budget, and preferences, facilitating structured data exchange for travel planning functionalities.

---
+++ src/main/java/com/aitrip/database/dto/PlanPageDTO.java
@@ -0,0 +1,37 @@
+package com.aitrip.database.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class PlanPageDTO {
+    private long id;
+
+    private String origin;
+
+    private String destination;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private BigDecimal budget;
+
+    private int adults;
+
+    private int children;
+
+    private String travelInterests;
+
+    private String flightPreference;
+
+    private String accomodation;
+
+    private String visaRequirements;
+
+    private String notes;
+}
