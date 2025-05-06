11407498f7a469c5dd6d668f3f8c03e2690939cb
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 21:47:18 +0300

    Add method to find Prompt by plan name in repository
    Added a new method `findByPlanName` to the `PromptRepository` interface. This allows fetching a `Prompt` entity based on its associated plan name, improving query capabilities.

--- src/main/java/com/aitrip/database/repository/PromptRepository.java
+++ src/main/java/com/aitrip/database/repository/PromptRepository.java
@@ -4,6 +4,9 @@ import com.aitrip.database.model.Prompt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PromptRepository extends JpaRepository<Prompt, Long> {
+    Optional<Prompt> findByPlanName(String planName);
 }
