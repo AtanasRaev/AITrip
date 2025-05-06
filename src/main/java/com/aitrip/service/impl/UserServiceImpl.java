9ecd3b1ecfb856ded86028796af51fb868b8bd28
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 15:03:22 +0300

    Add UserServiceImpl class implementation
    Introduce UserServiceImpl to implement the UserService interface. This sets up the service layer with the @Service annotation, adhering to Spring framework conventions. The file is a starting point for service logic implementation.

---
+++ src/main/java/com/aitrip/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.aitrip.service.impl;
+
+import com.aitrip.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
