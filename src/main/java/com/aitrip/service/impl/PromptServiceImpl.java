7b744eadc9e38a5723ad8878eb9b3446ab5749e9
AtanasRaev <atanaspraev@gmail.com>
2025-05-06 15:02:22 +0300

    Add PromptServiceImpl class implementation
    Introduce the PromptServiceImpl class to implement the PromptService interface. This addition sets up a service layer component, marked with the `@Service` annotation, for dependency injection in the application.

---
+++ src/main/java/com/aitrip/service/impl/PromptServiceImpl.java
@@ -0,0 +1,8 @@
+package com.aitrip.service.impl;
+
+import com.aitrip.service.PromptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PromptServiceImpl implements PromptService {
+}
