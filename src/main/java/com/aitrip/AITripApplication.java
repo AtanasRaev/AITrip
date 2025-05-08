d4a7327ef2572fff7b7200f838281d3b847df32b
AtanasRaev <atanaspraev@gmail.com>
2025-05-08 19:06:19 +0300

    Enable caching and fix class name casing in AITripApplication
    Added the @EnableCaching annotation to activate caching functionality in the application. Also corrected the class name from "AiTripApplication" to "AITripApplication" to maintain consistent naming conventions.

--- src/main/java/com/aitrip/AITripApplication.java
+++ src/main/java/com/aitrip/AITripApplication.java
@@ -2,12 +2,14 @@ package com.aitrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class AiTripApplication {
+@EnableCaching
+public class AITripApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AiTripApplication.class, args);
+        SpringApplication.run(AITripApplication.class, args);
     }
 
 }
