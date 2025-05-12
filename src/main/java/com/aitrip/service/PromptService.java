857e95bef1025c1d2ff8d4c347d5be4a2612b08c
AtanasRaev <atanaspraev@gmail.com>
2025-05-12 21:57:36 +0300

    Add CRUD methods to PromptService interface
    Implemented methods for creating, editing, retrieving by ID, and deleting prompts. These changes expand the service's functionality to fully manage prompts through standardized DTOs.

--- src/main/java/com/aitrip/service/PromptService.java
+++ src/main/java/com/aitrip/service/PromptService.java
@@ -1,7 +1,17 @@
 package com.aitrip.service;
 
+import com.aitrip.database.dto.PromptCreateDTO;
 import com.aitrip.database.dto.PromptDTO;
+import com.aitrip.database.dto.PromptEditDTO;
 
 public interface PromptService {
     PromptDTO getPromptByPlanName(String planName);
+
+    PromptDTO getPromptById(Long id);
+
+    PromptDTO createPrompt(PromptCreateDTO promptCreateDTO);
+
+    PromptDTO editPromptById(Long id, PromptEditDTO promptEditDTO);
+
+    Long deletePromptById(Long id);
 }
