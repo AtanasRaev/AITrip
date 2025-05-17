a8fd2e771c9923f40b5425be15489df66a9a60fa
AtanasRaev <atanaspraev@gmail.com>
2025-05-17 19:59:40 +0300

    Update AmadeusService method to fetch hotel offers.
    Renamed and modified the `getHotels` method to `getHotelOffers` to align with updated functionality. Changed the parameter to use `PlanCreateDTO` and updated the return type to `HotelOfferResponseDTO`.

--- src/main/java/com/aitrip/service/AmadeusService.java
+++ src/main/java/com/aitrip/service/AmadeusService.java
@@ -1,11 +1,11 @@
 package com.aitrip.service;
 
 import com.aitrip.database.dto.flight.response.FlightResponseDTO;
-import com.aitrip.database.dto.hotel.response.HotelResponseDTO;
+import com.aitrip.database.dto.hotel.response.offers.HotelOfferResponseDTO;
 import com.aitrip.database.dto.plan.PlanCreateDTO;
 
 public interface AmadeusService {
     FlightResponseDTO getFlights(PlanCreateDTO planCreateDTO);
 
-    HotelResponseDTO getHotels(String destination);
+    HotelOfferResponseDTO getHotelOffers(PlanCreateDTO planCreateDTO);
 }
