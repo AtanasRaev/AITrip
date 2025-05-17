52e986ae114f634cebfd22e89e5606a91ff088cb
AtanasRaev <atanaspraev@gmail.com>
2025-05-17 19:59:12 +0300

    Add HotelOfferResponseDTO for handling hotel offers data
    Introduce a new DTO class, `HotelOfferResponseDTO`, to structure and manage hotel offers response data. This includes a nested `HotelOfferDataDTO` class for encapsulating individual offer details, preparing the groundwork for hotel-related responses.

---
+++ src/main/java/com/aitrip/database/dto/hotel/response/offers/HotelOfferResponseDTO.java
@@ -0,0 +1,18 @@
+package com.aitrip.database.dto.hotel.response.offers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class HotelOfferResponseDTO {
+    private List<HotelOfferDataDTO> data;
+}
+
+@Getter
+@Setter
+class HotelOfferDataDTO {
+    // Fields representing hotel offer data
+}
