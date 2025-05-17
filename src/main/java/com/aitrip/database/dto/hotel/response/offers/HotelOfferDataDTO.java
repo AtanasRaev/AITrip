1f3846149fb2959a20ef7ab9be7a245c5fc5706b
AtanasRaev <atanaspraev@gmail.com>
2025-05-17 20:05:43 +0300

    Refactor DTO classes for hotel offer responses.
    Split `HotelOfferResponseDTO` into separate files for better structure and maintainability. Moved `HotelOfferDataDTO` to its own file to ensure clear separation of concerns.

---
+++ src/main/java/com/aitrip/database/dto/hotel/response/offers/HotelOfferDataDTO.java
@@ -0,0 +1,10 @@
+package com.aitrip.database.dto.hotel.response.offers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+class HotelOfferDataDTO {
+    // Fields representing hotel offer data
+}
