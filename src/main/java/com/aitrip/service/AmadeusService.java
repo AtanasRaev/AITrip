8abdfc9365a9068b6038df8931d5850822d7d7f7
AtanasRaev <atanaspraev@gmail.com>
2025-05-17 17:31:33 +0300

    Add methods to fetch flights and hotels in AmadeusService
    Introduced `getFlights` for retrieving flight details and `getHotels` for fetching hotel information. These methods enhance the service with capabilities for handling travel-related data.

--- src/main/java/com/aitrip/service/AmadeusService.java
+++ src/main/java/com/aitrip/service/AmadeusService.java
@@ -1,4 +1,11 @@
 package com.aitrip.service;
 
+import com.aitrip.database.dto.flight.response.FlightResponseDTO;
+import com.aitrip.database.dto.hotel.response.HotelResponseDTO;
+import com.aitrip.database.dto.plan.PlanCreateDTO;
+
 public interface AmadeusService {
+    FlightResponseDTO getFlights(PlanCreateDTO planCreateDTO);
+
+    HotelResponseDTO getHotels(String destination);
 }
