277366e4d44b0576d5b0799a03046b0e50213037
AtanasRaev <atanaspraev@gmail.com>
2025-05-23 02:31:25 +0300

    Add method to fetch paginated cities by country and query
    Introduced a new `findCitiesByCountryAndQuery` method in `CityServiceImpl` to support retrieving cities by country code and search query with pagination. This required integrating `CityRepository` and adding necessary dependencies like `Pageable`.

--- src/main/java/com/aitrip/service/impl/CityServiceImpl.java
+++ src/main/java/com/aitrip/service/impl/CityServiceImpl.java
@@ -1,9 +1,21 @@
 package com.aitrip.service.impl;
 
+import com.aitrip.database.dto.location.CityPageDTO;
+import com.aitrip.database.repository.CityRepository;
 import com.aitrip.service.CityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class CityServiceImpl implements CityService {
+    private final CityRepository cityRepository;
 
+
+    @Override
+    public Page<CityPageDTO> findCitiesByCountryAndQuery(String countryCode, String query, Pageable pageable) {
+        return this.cityRepository.findByCountryAndQuery(countryCode, query, pageable);
+    }
 }
