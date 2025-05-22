e7ef822c4f09c467cf3b1ea84e79f7d27100d73b
AtanasRaev <atanaspraev@gmail.com>
2025-05-23 00:36:57 +0300

    Add CityService and its implementation
    Introduce `CityService` interface and its implementation `CityServiceImpl`. Annotate `CityServiceImpl` with `@Service` to enable Spring's service layer functionality.

---
+++ src/main/java/com/aitrip/service/impl/CityServiceImpl.java
@@ -0,0 +1,9 @@
+package com.aitrip.service.impl;
+
+import com.aitrip.service.CityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CityServiceImpl implements CityService {
+
+}
