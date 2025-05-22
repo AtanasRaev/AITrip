927fcb5e74262fb050e8bfc22eda3d229cfd3579
AtanasRaev <atanaspraev@gmail.com>
2025-05-23 02:30:31 +0300

    Add CountryPageDTO for country pagination data
    Introduces a new DTO class, `CountryPageDTO`, to represent country data with `code` and `name` fields. This class includes Lombok annotations for getters, setters, and an all-args constructor.

---
+++ src/main/java/com/aitrip/database/dto/location/CountryPageDTO.java
@@ -0,0 +1,14 @@
+package com.aitrip.database.dto.location;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CountryPageDTO {
+    private String code;
+
+    private String name;
+}
