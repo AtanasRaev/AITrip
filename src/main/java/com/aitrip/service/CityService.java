a5207ea92e78ae33cb5a9db0afd8d59cf6bd4a32
AtanasRaev <atanaspraev@gmail.com>
2025-05-23 02:31:15 +0300

    Add method to fetch cities by country and query
    Introduced a new method in the CityService interface to retrieve a pageable list of cities based on a given country code and search query. This enhancement facilitates more dynamic and efficient city lookups.

--- src/main/java/com/aitrip/service/CityService.java
+++ src/main/java/com/aitrip/service/CityService.java
@@ -1,4 +1,9 @@
 package com.aitrip.service;
 
+import com.aitrip.database.dto.location.CityPageDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface CityService {
+     Page<CityPageDTO> findCitiesByCountryAndQuery(String countryCode, String query, Pageable pageable);
 }
