9a6ab3d6a98d58798e7078fe608c4a8f46b9efa9
AtanasRaev <atanaspraev@gmail.com>
2025-05-23 02:30:12 +0300

    Add CityPageDTO class for city data representation
    Introduce a new Data Transfer Object (DTO) class, `CityPageDTO`, to encapsulate city-related data. Includes fields for `name` and `iataCode` with appropriate Lombok annotations for boilerplate code reduction.

---
+++ src/main/java/com/aitrip/database/dto/location/CityPageDTO.java
@@ -0,0 +1,14 @@
+package com.aitrip.database.dto.location;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class CityPageDTO {
+    private String name;
+
+    private String iataCode;
+}
