dadfd5d2690777cfeec1d749c6efb47a226b8b63
AtanasRaev <atanaspraev@gmail.com>
2025-05-27 01:48:05 +0300

    Add environment parameter to createPlan endpoint
    Refactored the createPlan method in PlanController to accept an additional `environment` parameter of type AmadeusEnvironment. This allows the method to handle different environmental configurations when saving a plan. Updated necessary imports and annotations accordingly.

--- src/main/java/com/aitrip/web/PlanController.java
+++ src/main/java/com/aitrip/web/PlanController.java
@@ -1,13 +1,11 @@
 package com.aitrip.web;
 
+import com.aitrip.config.AmadeusEnvironment;
 import com.aitrip.database.dto.plan.PlanCreateDTO;
 import com.aitrip.service.PlanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -19,10 +17,11 @@ public class PlanController {
 
 
     @PostMapping("/create")
-    public ResponseEntity<Map<String, Object>> createPlan(@RequestBody PlanCreateDTO planCreateDTO) {
+    public ResponseEntity<Map<String, Object>> createPlan(@RequestBody PlanCreateDTO planCreateDTO,
+                                                          @RequestParam(value = "environment") AmadeusEnvironment environment) {
         return ResponseEntity.ok().body(Map.of(
                 "message", "success",
-                "plan", this.planService.savePlan(planCreateDTO))
+                "plan", this.planService.savePlan(planCreateDTO, environment))
         );
     }
 }
