3f4c75c2604a9c53bd072788eb09776b1ebab6a7
AtanasRaev <atanaspraev@gmail.com>
2025-05-27 01:48:14 +0300

    Update savePlan method to include AmadeusEnvironment parameter
    Modified the savePlan method in PlanService to accept an AmadeusEnvironment parameter. This change ensures the method can access necessary environmental configurations for improved functionality.

--- src/main/java/com/aitrip/service/PlanService.java
+++ src/main/java/com/aitrip/service/PlanService.java
@@ -1,8 +1,9 @@
 package com.aitrip.service;
 
+import com.aitrip.config.AmadeusEnvironment;
 import com.aitrip.database.dto.plan.PlanCreateDTO;
 import com.aitrip.database.dto.plan.PlanPageDTO;
 
 public interface PlanService {
-    PlanPageDTO savePlan(PlanCreateDTO planCreateDTO);
+    PlanPageDTO savePlan(PlanCreateDTO planCreateDTO, AmadeusEnvironment environment);
 }
