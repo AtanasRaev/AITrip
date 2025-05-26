fbf06f14da45afbb05a42061dfbc571e0f97a318
AtanasRaev <atanaspraev@gmail.com>
2025-05-27 01:47:30 +0300

    Refactor createPlan to include flights and hotel offers
    Updated the createPlan method in OpenAIService to accept additional parameters for flight and hotel offers. This change enables the service to generate plans with richer travel details, improving functionality and flexibility.

--- src/main/java/com/aitrip/service/OpenAIService.java
+++ src/main/java/com/aitrip/service/OpenAIService.java
@@ -2,7 +2,9 @@ package com.aitrip.service;
 
 import com.aitrip.database.dto.plan.PlanCreateDTO;
 import com.aitrip.database.dto.plan.PlanPageDTO;
+import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.HotelOffer;
 
 public interface OpenAIService {
-    PlanPageDTO createPlan(PlanCreateDTO planCreateDTO);
+    PlanPageDTO createPlan(PlanCreateDTO planCreateDTO, FlightOfferSearch[] flights, HotelOffer[] hotelOffers);
 }
